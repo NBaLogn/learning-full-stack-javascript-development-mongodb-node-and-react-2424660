1ae0f62e1844aecc7cef96a503af34baf23c4e3b
NBaLogn <36346192+NBaLogn@users.noreply.github.com>
2023-07-14 21:22:38 -0400

    middleware and templating

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,3 +1,19 @@
 import express from 'express';
 
-console.log(express);
+const server = express();
+
+server.use(express.static("dist"));
+
+server.set("view engine", "ejs");
+
+server.use("/", (req, res) => {
+  res.render("index", {
+    content: "<em>EJS</em> is cool",
+  });
+});
+
+server.listen(8080, "0.0.0.0", () => {
+  console.info(
+    "Express serve is listening at http://0.0.0.0:8080",
+  );
+});

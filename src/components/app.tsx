c686250be99ed847ad007b21183241088615b184
NBaLogn <36346192+NBaLogn@users.noreply.github.com>
2023-07-15 21:17:02 -0400

    components life cycle and side effects

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,20 +1,10 @@
 import Header from "./header";
-import { useState } from "react";
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-
   return (
     <>
       <div className="container">
-        <Header message="Naming Contests" />
-        <button
-          onClick={() => {
-            setCounter(counter + 1);
-          }}
-        >
-          {counter}
-        </button>
+        <Header message="Naming ContestXXX" />
       </div>
     </>
   );

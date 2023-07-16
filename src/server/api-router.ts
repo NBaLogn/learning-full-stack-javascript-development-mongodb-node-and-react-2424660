75be9210558cf440aee67962d879dc7f3de69f60
NBaLogn <36346192+NBaLogn@users.noreply.github.com>
2023-07-16 14:32:38 -0400

    fetching data from mongodb

--- src/server/api-router.ts
+++ src/server/api-router.ts
@@ -1,16 +1,35 @@
 import express from "express";
 import cors from "cors";
+import { connectClient } from "./db";
+import { Db } from "mongodb";
 
 const router = express.Router();
 router.use(cors());
+router.get("/contests", async (req, res) => {
+  const client: Db = await connectClient();
 
-import testData from "../test-data.json";
-
-router.get("/contests", (req, res) => {
-  // get the data from MongoDB
-  res.send({ contests: testData });
+  const contests = await client
+    .collection("contests")
+    .find()
+    .project({
+      _id: 0,
+      id: 1,
+      categoryName: 1,
+      contestName: 1,
+    })
+    .toArray();
+  res.send({ contests });
 });
 
-// router.get("/contest")
+router.get("/contest/:contestId", async (req, res) => {
+  const client: Db = await connectClient();
+
+  console.log(req.params.contestId);
+  const contest = await client
+    .collection("contests")
+    .findOne({ id: req.params.contestId });
+
+  res.send({ contest });
+});
 
 export default router;

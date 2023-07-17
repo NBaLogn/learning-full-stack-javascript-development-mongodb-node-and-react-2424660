a331e095fcdc7970eb300e616d2bd13092982221
NBaLogn <36346192+NBaLogn@users.noreply.github.com>
2023-07-17 13:15:58 -0400

    handling browser navigation history

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import App from './components/app';
 const container = document.getElementById("app");
 const root = createRoot(container);
 
-
 root.render(
   <App initialData={(window as any).initialData} />,
 );
